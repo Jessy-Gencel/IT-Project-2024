3110816bf3961b2caf75c2d2916560e3e9131588
Primordial-Noir <jessy.gencel@student.ehb.be>
2024-10-06 20:42:40 +0200

    Added debug setup vscode

--- Backend/matching/views.py
+++ Backend/matching/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
+    random_variable = 1
+    second_variable = 2
     return render(request, 'hello.html', {'name': 'Primordial'})
