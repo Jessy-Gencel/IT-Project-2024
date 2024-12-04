d42f0be5d84d60bb0e027698ea04ea8994d6ba51
Tome <cecepas2001@gmail.com>
2024-12-04 12:26:14 +0100

    style: theme, fonts, typography setup

---
+++ Frontend/theme/fonts.js
@@ -0,0 +1,10 @@
+const fonts = {
+  regular: "Inter_400Regular",
+  medium: "Inter_500Medium",
+  semiBold: "Inter_600SemiBold",
+  bold: "Inter_700Bold",
+  light: "Inter_300Light",
+  extraBold: "Inter_800ExtraBold",
+};
+
+export default fonts;
---
+++ Frontend/theme/typography.js
@@ -0,0 +1,31 @@
+import fonts from './fonts';
+
+const typography = {
+  body: {
+    fontFamily: fonts.regular,
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  title: {
+    fontFamily: fonts.bold,
+    fontSize: 24,
+    lineHeight: 32,
+  },
+  subtitle: {
+    fontFamily: fonts.medium,
+    fontSize: 18,
+    lineHeight: 26,
+  },
+  caption: {
+    fontFamily: fonts.light,
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  button: {
+    fontFamily: fonts.semiBold,
+    fontSize: 14,
+    lineHeight: 20,
+  },
+};
+
+export default typography;
