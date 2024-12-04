f8977e9ad0f47fc8c57c958a6629a8144db2065a
Luna D'Heere <luna.dheere@student.ehb.be>
2024-12-04 16:22:49 +0100

    style: login pagina stijl toegepast

---
+++ Frontend/styles/LogIn.js
@@ -0,0 +1,34 @@
+const styles = {
+    logInContainer:{
+        marginLeft: 30,
+        marginRight: 30,
+        backgroundColor:'#fbb03b'
+    },
+    logInText:{
+        fontWeight: "bold",
+        fontSize: 20,
+        color: '#353535',
+    },
+    inputTitle:{
+        color: '#3535358C',
+        fontSize: 12,
+    },
+
+    //input boxes
+
+    input: {
+        height: 40,
+        borderColor: '#3535358C',
+        borderWidth: 1,
+        borderRadius: 5, 
+        paddingLeft: 10, 
+        marginVertical: 5,
+        fontSize: 16, 
+        backgroundColor: '#fff', 
+      },
+
+      //GateWay en logo
+      
+
+}
+export default styles;
