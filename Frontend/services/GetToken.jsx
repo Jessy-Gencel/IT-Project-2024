846a5be8679f1200640e123ec8dc9c9180540749
nils.kelecom <145758921+nilskele@users.noreply.github.com>
2024-12-19 14:01:09 +0100

    get token dynamically

--- Frontend/services/GetToken.jsx
+++ Frontend/services/GetToken.jsx
@@ -9,7 +9,19 @@ const getToken = async (key) => {
     return null;
   }
 };
-export {getToken};
+const getAuthTokens = async () => {
+  try {
+    const accessToken = await getToken("accessToken");
+    const refreshToken = await getToken("refreshToken");
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    console.error("Error retrieving tokens:", error);
+    return { accessToken: null, refreshToken: null };
+  }
+};
+
+export {getToken, getAuthTokens};
 
 // Example usage
 //const accessToken = await getToken("accessToken");
