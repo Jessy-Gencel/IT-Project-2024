e28518fd729edd8370001fde54b9534d653f7cdf
Tome <cecepas2001@gmail.com>
2024-12-19 09:57:21 +0100

    Update matching card design for home screen

--- Frontend/components/MatchingCard.jsx
+++ Frontend/components/MatchingCard.jsx
@@ -21,23 +21,52 @@ const MatchingCard = ({
       <Image source={src} style={styles.image} />
 
       <View style={styles.cardInfo}>
-        <View>
-          <Text>
+        {/* Name */}
+        <View style={styles.nameContainer}>
+          <Text style={styles.name}>
             {name}, {age}
           </Text>
         </View>
-        <View style={styles.badgeList}>
-          {interests.map((title) => {
-            return <Badge title={title} />;
-          })}
+
+        {/* Progress Bar */}
+        <View>
+          <Text>{matchPercentage}%</Text>
+          <ProgressBar
+            fillWidth={matchPercentage}
+            height={15}
+            borderRadius={10}
+            barColor="#5F63E2"
+          />
         </View>
-        <Text>{matchPercentage}%</Text>
-        <ProgressBar
-          fillWidth={matchPercentage}
-          height={15}
-          borderRadius={10}
-          barColor="#5F63E2"
-        />
+
+        {/* Badges */}
+        {/* Interests */}
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeTitle}>Interests</Text>
+          <View style={styles.badgeList}>
+            {interests.map((title) => {
+              return <Badge title={title} />;
+            })}
+          </View>
+        </View>
+        {/* Hobbies */}
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeTitle}>Hobbies</Text>
+          <View style={styles.badgeList}>
+            {interests.map((title) => {
+              return <Badge title={title} />;
+            })}
+          </View>
+        </View>
+        {/* Books */}
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeTitle}>Books</Text>
+          <View style={styles.badgeList}>
+            {interests.map((title) => {
+              return <Badge title={title} />;
+            })}
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -46,12 +75,29 @@ const MatchingCard = ({
 const styles = {
   card: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: "column",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: colors.placeholder,
   },
   image: {
     height: 250,
     width: 250,
     resizeMode: "contain",
+  },
+  cardInfo: {
+    padding: 10,
+    gap: 10,
+  },
+  nameContainer: {},
+  name: {
+    fontWeight: "medium",
+    fontSize: 24,
+  },
+  badgeList: {
+    flexDirection: "row",
+    gap: 5,
+    paddingVertical: 15,
   },
 };
 
