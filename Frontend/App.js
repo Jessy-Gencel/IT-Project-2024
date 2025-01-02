007fe410836944a1e6ce8a81530f08b3765effc3
Luna D'Heere <luna.dheere@student.ehb.be>
2025-01-02 14:04:06 +0100

    register link works now

--- Frontend/App.js
+++ Frontend/App.js
@@ -5,7 +5,6 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import GatewaysScreen from './screens/GatewaysScreen';
-import Test from './screens/Test';
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -15,6 +14,7 @@ import AccountSetupScreen from './screens/AccountSetupScreen';
 import BottomTabNavigator from "./components/BottomTabNavigator";
 import LogInScreen from "./screens/LogInScreen";
 import HomePage from  "./screens/HomeScreen";
+import RegisterScreen from './screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
@@ -27,6 +27,7 @@ export default function App() {
         <Stack.Screen name="HomeScreen" component={HomePage} />
         <Stack.Screen name="Main" component={BottomTabNavigator} />
         <Stack.Screen name="AccountSetup" component={AccountSetupScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
