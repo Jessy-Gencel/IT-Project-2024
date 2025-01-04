f9fd9fd4135918a53f5bc60c989dc0dd00567682
Primordial-Noir <jessy.gencel@student.ehb.be>
2025-01-04 13:28:50 +0100

    MBTI small bug fix

--- Backend/Routes/auth_routes.py
+++ Backend/Routes/auth_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify,send_from_directory
 from Utils.password_hashing import hash_password, verify_password
 from Utils.jwt_encode import jwt_full_encode, token_refresh
 from Utils.sanitize_input import sanitize_input, santize_array
-from Services.couchbase_reads import find_user_by_email,find_user_by_id
+from Services.couchbase_reads import find_user_by_email,find_user_by_id,find_profile_by_id
 from Services.couchbase_writes import store_user,store_profile
 from Utils.extract_name import extract_name
 from Services.embedding import embed_MiniLM
@@ -98,9 +98,10 @@ def create_profile():
     blob.upload_from_filename(f"DB/PFP/{pfp_url}")
     print(pfp_url)
     ############################## HANDLE IMAGE UPLOAD ###############################
+    traits_for_embedding = {"mbti" : mbti, "interest" : interests, "hobby" : hobbies, "game" : games, "movie" : movies, "book" : books, "music" : music}
     traits = {"mbti" : mbti, "interest" : interests, "hobby" : hobbies, "game" : games, "movie" : movies, "book" : books, "music" : music}
     ############################## MAKE TRAITS DICT ###############################
-    predefined_matching_categories = embed_MiniLM(int(id),traits)
+    predefined_matching_categories = embed_MiniLM(int(id),traits_for_embedding)
     print(predefined_matching_categories)
     ############################## MAKE VECTORS FOR PROFILE ###############################
     trait_vectors = predefined_matching_categories
@@ -110,6 +111,22 @@ def create_profile():
     profile = store_profile(user_profile)
     print(profile)
     return "User created correctly", 200
+
+@auth_bp.route('/profile/edit', methods=['POST'])
+@token_required
+def edit_profile(payload):
+    data = request.get_json()
+    id = sanitize_input(str(data['id']))
+    mbti = sanitize_input(str(data['mbti']))
+    interests = santize_array(data['interests'])
+    hobbies = santize_array(data['hobbies'])
+    games = santize_array(data['games'])
+    movies = santize_array(data['movies'])
+    books = santize_array(data['books'])
+    music = santize_array(data['music'])
+    initial_profile = find_profile_by_id(id)
+    ############################## SANITIZATION ###############################
+
 
 @auth_bp.route('/refresh', methods=['POST'])
 def refresh():
@@ -133,12 +150,12 @@ def get_profiles():
     return "Yippie profiles", 200
 
 @auth_bp.route('/profiles/<profile_id>', methods=['GET'])
-def get_profile(profile_id):
-    # profile = find_profile_by_id(profile_id)
-    # if profile:
-    #     return jsonify(profile), 200
-    # else:
-    #     return jsonify({"error": "Profile not found"}), 404
-    return "Yippie profiles/profile_id", 200
+@token_required
+def get_profile(payload,profile_id):
+    profile = find_profile_by_id(profile_id)
+    if profile:
+       return jsonify(profile), 200
+    else:
+        return jsonify({"error": "Profile not found"}), 404
 
 
